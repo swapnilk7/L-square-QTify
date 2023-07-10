f9f3ad06b55bf15050366cd00d31fc1e39e41199
Swapnil Khedkar <swapnilkhedkar@valuebound.com>
2023-07-10 17:31:33 +0530

    hero-qtify-takehome

---
+++ qtify/src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Headphone from "../../assets/vibrating-headphone 1.png";
+import styles from "./HeroSection.module.css";
+
+const HeroSection = () => {
+  return (
+    <div className={styles.mainContainer}>
+      <div>
+        <p className={styles.heroText}>100 Thousand Songs, ad-free</p>
+        <p className={styles.heroText}>Over thousands podcast episodes</p>
+      </div>
+      <img src={Headphone} alt="headphone" width={212} height={212} />
+    </div>
+  );
+};
+
+export default HeroSection;
